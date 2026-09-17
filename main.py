44c084e994ddcec890ac8a53c44c97f16c7f661d
isaac <moraisaac3006@gmail.com>
2026-09-16 18:24:32 -0600

    menu de opciones

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hola")
+opcion= 0
+
+while opcion != 5:
+    
+    print("\n====================================")
+    print("CONTROL DE VENTAS DE VIDEOJUEGOS")
+    print("\n====================================")
+     
+    print("1.Agregar juego")
+    print("2.Ver cantidad de juegos")
+    print("3.ver precio")
+    print("4.eliminar juego")
+    print("5.salir")
+    
+    opcion=int(input("ingrese la opcion a la que quiera ingresar: "))
+    
