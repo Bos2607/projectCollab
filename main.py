1daa81a67dab1ecf27b527b04bff88a161f1df99
Byron <obandob868@gmail.com>
2026-09-23 23:00:47 -0600

    Creacion de la funcion agregar_juego

--- main.py
+++ main.py
@@ -7,6 +7,57 @@ juegos_en_progreso=[]
 juegos_platinados=[]
 
 
+
+
+def agregar_juego():
+    while True:
+        try:
+            size=int(input("Ingrese la cantidad de juegos a agregar: "))
+            if size <= 0:
+                print("Ingrese numero valido")
+                continue
+            break
+        except ValueError:
+            print("Ingrese solo numeros: ")
+            continue
+        
+    for j in range(size):
+        nombre=input("Ingrese nombre de juego: ")
+            
+        plataforma=input("Ingrese nombre de plataforma del juego: ")
+            
+        estado=input("Ingrese estado del juego (Terminado, Progreso o Platinado): ").lower()
+        
+        
+        
+        genero=input("Ingrese genero del juego: ")
+        
+        while True:
+          try:    
+              precio=float(input(f"Ingrese precio de {nombre}: "))
+              if precio <= 0:
+                 print("Precio invalido")
+                 continue
+              break
+          except ValueError:
+              print("Ingrese solo numeros")
+              continue
+        
+    
+        if estado == "terminado":
+            juegos_Terminados.append(nombre)
+                
+        elif estado == "progreso":
+           juegos_en_progreso.append(nombre)
+            
+        elif estado == "platinado":
+           juegos_platinados.append(nombre)
+           juegos_Terminados.append(nombre)
+        
+    print(f"{nombre} agregado exitosamente")
+        
+
+
 opcion= 0
 
 while opcion != 5:
@@ -25,7 +76,10 @@ while opcion != 5:
     
     opcion=int(input("ingrese la opcion a la que quiera ingresar: "))
     
-    print("verificar")
-    
+    if opcion == 1:
+        agregar_juego()
 
+        
+        
+       
 
